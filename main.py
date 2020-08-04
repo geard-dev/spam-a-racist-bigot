20160581382fac666c909945dd6a8e857b1dc743
geard-dev <gedhiamandi@icloud.com>
2020-08-04 00:11:27 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+x = 1
+
+def spam():
+    while x == 1:
+        pyautogui.write("Hey racist bitch who buys shit that was made in China 😹👎🏼 I hope your chav ass highlighter and contour is toxic 😜✨ black lives matter bitch 🚶🏻‍♀️\n")
+
+spam()
